0d9d2311a591b3e532a929d78234b06a6ca636e1
Akash KUmar <kumar.ryan94@gmail.com>
2021-07-19 18:42:47 +0530

    add stock model and iex api key

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'welcome#index'
+  devise_for :users
+
+  devise_scope :user do  
+     get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  
+end
