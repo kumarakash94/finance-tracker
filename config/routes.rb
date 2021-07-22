fa7850231bfbf85905efcf860aa62bc316a351b1
Akash KUmar <kumar.ryan94@gmail.com>
2021-07-22 19:12:37 +0530

    adding javascript and ajax function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
+
   devise_for :users
 
   devise_scope :user do  
